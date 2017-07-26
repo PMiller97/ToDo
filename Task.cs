f89d46caa40b23131678379f0c7dc55101b48b9c
Paige Miller <paige_9753@hotmail.com>
2017-07-26 09:45:14 -0400

    inital commit

---
+++ Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDo
+{
+    public class Task
+    {
+        [Key]
+        public int TaskID { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime DueDate { get; set; }
+
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
+    }
+}
